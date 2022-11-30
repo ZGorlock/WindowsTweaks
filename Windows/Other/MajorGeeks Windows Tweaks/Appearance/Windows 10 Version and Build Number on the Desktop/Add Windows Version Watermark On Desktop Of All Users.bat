a5dd977123dbb325a8a40615fe5e88e057909f88
Zachary Gill <zachary.w.gill@gmail.com>
2022-11-29 20:05:46 -0500

    Compressed MajorGeeks Windows Tweaks 2.93

--- Windows/Other/MajorGeeks Windows Tweaks/Appearance/Windows 10 Version and Build Number on the Desktop/Add Windows Version Watermark On Desktop Of All Users.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /V DisplayVersion /T REG_DWORD /D 1 /F
-
-taskkill /f /im explorer.exe
-start explorer.exe
--- Windows/Other/MajorGeeks Windows Tweaks/Appearance/Windows 10 Version and Build Number on the Desktop/Remove Windows Version Watermark On Desktop Of Current User.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REG ADD "HKCU\Control Panel\Desktop" /V PaintDesktopVersion /T REG_DWORD /D 0 /F
-
-taskkill /f /im explorer.exe
-start explorer.exe
