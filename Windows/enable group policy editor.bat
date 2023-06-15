a4971f3443f79d9d68e2aedbb21d427d5bd341d2
Zachary Gill <zachary.w.gill@gmail.com>
2023-06-14 23:18:04 -0400

    Updated 'Windows' tweaks

---
+++ Windows/enable group policy editor.bat
@@ -0,0 +1,11 @@
+@echo off
+pushd "%~dp0"
+
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
+pause
+
+
+:: https://www.ghacks.net/2019/01/07/how-to-enable-gpedit-msc-group-policy-on-windows-10-home-devices/
