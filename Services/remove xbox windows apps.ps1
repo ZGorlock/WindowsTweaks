9cd615312c097d2ac5054768b17564c45beab85e
Zachary Gill <zachary.w.gill@gmail.com>
2023-06-14 23:14:16 -0400

    Updated batch scripts

---
+++ Services/remove xbox windows apps.ps1
@@ -0,0 +1,4 @@
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+
+
+# https://answers.microsoft.com/en-us/windows/forum/all/how-do-i-turn-off-all-services-for-xbox-on-my/86c67fd6-d3fb-4989-a5fc-b9afd35a5143
---
+++ Windows/reset windows security settings to default.bat
@@ -0,0 +1,4 @@
+secedit /configure /cfg %windir%\inf\defltbase.inf /db defltbase.sdb /verbose
+
+
+:: https://www.thewindowsclub.com/how-to-reset-windows-security-settings-to-its-defaults
