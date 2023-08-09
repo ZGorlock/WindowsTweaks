e2b91a3f66a1c237a416c207517a890c76bbfdba
Zachary Gill <zachary.w.gill@gmail.com>
2023-08-08 22:55:06 -0400

    Updated tplink scripts

--- Scripts/bat/tplink/tplink_toggle.bat
+++ Scripts/bat/tplink/tplink_toggle.bat
@@ -7,7 +7,6 @@ echo TP-Link Toggle
 echo --------------------
 echo --------------------
 
-
 set url=https://use1-wap.tplinkcloud.com
 
 set username=%~1
@@ -18,16 +17,13 @@ set deviceId=%~4
 
 set toggle=%~5
 
-
 echo url          = %url%
 echo username     = %username%
 echo password     = %password%
 echo uuid         = %uuid%
 echo toggle       = %toggle%
 
-
 if "%toggle%"=="off" (set state=0) else (set state=1)
-
 
 echo --------------------
 for /f "tokens=1 delims=~" %%a in ('curl -X POST -H "Content-Type: application/json" -d "{ \"method\" : \"login\", \"params\" : { \"appType\" : \"Kasa_Android\", \"cloudPassword\" : \"%password%\", \"cloudUserName\" : \"%username%\", \"terminalUUID\" : \"%uuid%\" } }" %url%') do set token_curl=%%a
@@ -46,7 +42,6 @@ echo nickname     = %nickname%
 echo email        = %email%
 echo token        = %token%
 
-
 :: echo --------------------
 :: for /f "tokens=1 delims=" %%a in ('curl -X POST -H "Content-Type: application/json" -d "{ \"method\" : \"getDeviceList\" }" "%url%?token=%token%"') do set deviceId_curl=%%a
 :: echo %deviceId_curl%
@@ -54,13 +49,9 @@ echo token        = %token%
 
 echo deviceId     = %deviceId%
 
-
 echo --------------------
 for /f "tokens=1 delims=~" %%a in ('curl -X POST -H "Content-Type: application/json" -d "{ \"method\" : \"passthrough\", \"params\" : { \"deviceId\" : \"%deviceId%\", \"requestData\" : \"{ \\\"system\\\" : { \\\"set_relay_state\\\" : { \\\"state\\\" : %state% } } }\" } }" "%url%?token=%token%"') do set passthrough_curl=%%a
 echo %passthrough_curl%
 echo --------------------
-
-
-endlocal
 
 echo Done
