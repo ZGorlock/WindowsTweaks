adc98f90125166fc1f768a30b276eccd281f18df
Zachary Gill <zachary.w.gill@gmail.com>
2023-09-16 08:40:12 -0400

    Updated dl Script

--- Scripts/bat/dl/dl.bat
+++ Scripts/bat/dl/dl.bat
@@ -1,12 +1,12 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set url="%~1"
+set url=%~1
 
-set out="%~2"
-if '%out%'=='' (set "out=%DLOutput%")
-if '%out%'=='' (set "out=%Downloads%\.dl")
-if '%out%'=='' (set "out=%UserProfile%\Downloads\.dl")
+set out=%~2
+if '!out!'=='' (set "out=%DLOutput%")
+if '!out!'=='' (set "out=%Downloads%\.dl")
+if '!out!'=='' (set "out=%UserProfile%\Downloads\.dl")
 
 set env=dl
 
@@ -14,12 +14,12 @@ echo.
 echo --------------------------------------------------
 echo.
 
-if %url%==""       (goto :end)
-if %url%=="start"  (goto :listen)
-if %url%=="server" (goto :listen)
-if %url%=="listen" (goto :listen)
-if %url%=="pop"    (goto :open)
-if %url%=="open"   (goto :open)
+if '!url!'==''       (goto :end)
+if '!url!'=='start'  (goto :listen)
+if '!url!'=='server' (goto :listen)
+if '!url!'=='listen' (goto :listen)
+if '!url!'=='pop'    (goto :open)
+if '!url!'=='open'   (goto :open)
 
 call :download
 goto :end
@@ -27,23 +27,23 @@ goto :end
 
 :download
 	set video=false
-	if not '%url%'=='%url:/video=%'      (set video=true)
-	if not '%url%'=='%url:/view_video=%' (set video=true)
-	if not '%url%'=='%url:/watch=%'      (set video=true)
-	if not '%url%'=='%url:/playlist=%'   (set video=true)
-	if not '%url%'=='%url:/episode=%'    (set video=true)
+	if not '!url!'=='!url:/video=!'      (set video=true)
+	if not '!url!'=='!url:/view_video=!' (set video=true)
+	if not '!url!'=='!url:/watch=!'      (set video=true)
+	if not '!url!'=='!url:/playlist=!'   (set video=true)
+	if not '!url!'=='!url:/episode=!'    (set video=true)
 	
-	if not exist %out% (mkdir %out%)
-	cd /D %out%
+	if not exist "!out!" (mkdir "!out!")
+	cd /D "!out!"
 	
 	if not '%env%'=='' (call conda activate %env%)
 	
-	if "%video%"=="true" (
+	if '%video%'=='true' (
 		echo Using yt-dlp...
-		yt-dlp -f mp4 -S res:720 %url%
+		yt-dlp -f mp4 -S res:720 -o "!out!\%%(title)s.%%(ext)s" "!url!"
 	) else (
 		echo Using gallery-dl...
-		gallery-dl --verbose --dest %out% %url%
+		gallery-dl --verbose --dest "!out!" "!url!"
 	)
 	
 	if not '%env%'=='' (call conda deactivate)
@@ -52,11 +52,10 @@ goto :end
 
 
 :listen
-	set url=""
+	set url=
 	set /p url=: 
-	set url="%url:"=%"
 	
-	if %url%=="" (goto :end)
+	if '!url!'=='' (goto :end)
 	
 	call :download
 	
@@ -68,9 +67,9 @@ goto :end
 
 
 :open
-	if not exist %out% (mkdir %out%)
+	if not exist "!out!" (mkdir "!out!")
 	
-	explorer %out%
+	explorer "!out!"
 	
 	goto :end
 
