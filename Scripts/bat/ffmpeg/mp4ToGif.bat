4894af189725866b2aaedf5704d767a0dc45cfd9
Zachary Gill <zachary.w.gill@gmail.com>
2023-09-26 06:17:45 -0400

    Updated mp4ToGif ffmpeg Script

--- Scripts/bat/ffmpeg/mp4ToGif.bat
+++ Scripts/bat/ffmpeg/mp4ToGif.bat
@@ -3,6 +3,25 @@ setlocal enabledelayedexpansion
 
 set file=%~1
 
+if "%file:~0,1%"=="-" (
+	set mode=%file:~1%
+	set file=%~2
+)
+
+set fps=15
+if "%mode%"=="1" (set fps=10)
+if "%mode%"=="2" (set fps=10)
+if "%mode%"=="3" (set fps=15)
+if "%mode%"=="4" (set fps=20)
+if "%mode%"=="5" (set fps=20)
+
+set dim=720
+if "%mode%"=="1" (set dim=360)
+if "%mode%"=="2" (set dim=480)
+if "%mode%"=="3" (set dim=720)
+if "%mode%"=="4" (set dim=1080)
+if "%mode%"=="5" (set dim=9999)
+
 if "%file%"=="" (
 	for %%f in (*.mp4) do (call :mp4ToGif "%%f")
 ) else (
@@ -32,13 +51,14 @@ goto :end
 		echo Converting: !mp4!
 		echo         to: !gif!
 		
-		set "frate=fps=20"
+		set "frate=fps=!fps!"
 REM		set "frate=mpdecimate,setpts=N/FRAME_RATE/TB"
 		
 REM		set "scale=scale=-1:-1:flags=lanczos"
-		set "scale=scale=if(gte(iw\,ih)\,min(1280\,iw)\,-2):if(lt(iw\,ih)\,min(1280\,ih)\,-2):flags=lanczos"
+		set "scale=scale=if(gte(iw\,ih)\,min(!dim!\,iw)\,-2):if(lt(iw\,ih)\,min(!dim!\,ih)\,-2):flags=lanczos"
 		
 		set "split=split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
+		
 		set filter=!frate!,!scale!,!split!
 		
 		set ffmpeg_cmd=ffmpeg -hide_banner -i !mp4! -vf "!filter!" -loop 0 -y !gif!
