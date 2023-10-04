1c206370e9d0eaef61dba97bb8874ce04a830cde
Zachary Gill <zachary.w.gill@gmail.com>
2023-10-04 02:51:10 -0400

    Updated enum cmd Script

--- Scripts/bat/cmd/enumerate.bat
+++ Scripts/bat/cmd/enumerate.bat
@@ -1,12 +1,18 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-if '%~1'=='' (set pattern=*) else (set "pattern=%~1")
-if '%~2'=='' (set output=enum.txt) else (set "output=%~2")
+if '%~1'=='' (set "pattern=*") else (set "pattern=%~1")
+if '%~2'=='' (set "output=enum.txt") else (set "output=%~2")
 
-if exist "%output%" if /i not '%~4'=='/y' (
-	choice /c yn /n /m "Output file '%output%' already exists. Overwrite? [y/n]"
-	if ERRORLEVEL 2 (exit /b 1)
+if exist "!output!" (
+	if /i not '%~4'=='/y' (
+		choice /c yn /n /m "Output file '!output!' already exists. Overwrite? [y/n]"
+		if ERRORLEVEL 2 (exit /b 1)
+	)
+	del "!output!"
 )
 
-locate %~3 "%pattern%" > "%output%"
+call touch "!output!"
+for /f "tokens=*" %%x in ('locate %~3 "!pattern!"') do (
+	if not "%%x"=="!output!" (echo %%x >> "!output!")
+)
