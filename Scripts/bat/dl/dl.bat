7e598ce7be367be836632dfd1cc63072771d4efc
Zachary Gill <zachary.w.gill@gmail.com>
2023-10-08 05:45:19 -0400

    Updated dl Script

--- Scripts/bat/dl/dl.bat
+++ Scripts/bat/dl/dl.bat
@@ -1,28 +1,102 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set url=%~1
+set "url=%~1"
 
-set out=%~2
+set "out=%~2"
 if '!out!'=='' (set "out=%DLOutput%")
 if '!out!'=='' (set "out=%Downloads%\.dl")
 if '!out!'=='' (set "out=%UserProfile%\Downloads\.dl")
 
 set env=dl
 
+set listen=false
+
 echo.
 echo --------------------------------------------------
 echo.
 
-if '!url!'==''       (goto :end)
-if '!url!'=='start'  (goto :listen)
-if '!url!'=='server' (goto :listen)
-if '!url!'=='listen' (goto :listen)
-if '!url!'=='pop'    (goto :open)
-if '!url!'=='open'   (goto :open)
+goto :main
 
-call :download
-goto :end
+
+:main
+	call :command
+	call :process
+	
+	goto :input
+
+
+:input
+	if not '!listen!'=='true' (goto :end)
+	
+	echo.
+	echo --------------------------------------------------
+	echo.
+	
+	set url=
+	set /p url=: 
+	
+	goto :main
+
+
+:command
+	if '!url!'=='start'   (call :start)
+	if '!url!'=='server'  (call :start)
+	if '!url!'=='listen'  (call :start)
+	if '!url!'=='loop'    (call :start)
+	
+	if '!url!'=='stop'    (call :stop)
+	if '!url!'=='quit'    (call :stop)
+	if '!url!'=='exit'    (call :stop)
+	if '!url!'=='end'     (call :stop)
+	if '!url!'=='break'   (call :stop)
+	if '!url!'==''        (call :stop)
+	
+	if '!url!'=='open'    (call :open)
+	if '!url!'=='explore' (call :open)
+	if '!url!'=='pop'     (call :open)
+	if '!url!'=='dir'     (call :open)
+	
+	exit /b 0
+
+
+:start
+	if '!listen!'=='true' (exit /b 1)
+	
+	echo Listening...
+	set listen=true
+	
+	exit /b 0
+
+
+:stop
+	if '!listen!'=='false' (exit /b 1)
+	
+	echo Done
+	set listen=false
+	
+	exit /b 0
+
+
+:open
+	if not exist "!out!" (mkdir "!out!")
+	
+	echo !out!
+	explorer "!out!"
+	
+	exit /b 0
+
+
+:process
+	if '!url!'==''          (exit /b 1)
+	if '!url!'=='!url://=!' (exit /b 1)
+	
+	if not exist "!out!" (mkdir "!out!")
+	cd /D "!out!"
+	
+	call :download
+	
+	exit /b 0
 
 
 :download
@@ -33,12 +107,9 @@ goto :end
 	if not '!url!'=='!url:/playlist=!'   (set video=true)
 	if not '!url!'=='!url:/episode=!'    (set video=true)
 	
-	if not exist "!out!" (mkdir "!out!")
-	cd /D "!out!"
+	if not '!env!'=='' (call conda activate !env!)
 	
-	if not '%env%'=='' (call conda activate %env%)
-	
-	if '%video%'=='true' (
+	if '!video!'=='true' (
 		echo Using yt-dlp...
 		yt-dlp -f mp4 -S res:720 -o "!out!\%%(title)s.%%(ext)s" "!url!"
 	) else (
@@ -46,32 +117,9 @@ goto :end
 		gallery-dl --verbose --dest "!out!" "!url!"
 	)
 	
-	if not '%env%'=='' (call conda deactivate)
+	if not '!env!'=='' (call conda deactivate)
 	
 	exit /b 0
-
-
-:listen
-	set url=
-	set /p url=: 
-	
-	if '!url!'=='' (goto :end)
-	
-	call :download
-	
-	echo.
-	echo --------------------------------------------------
-	echo.
-	
-	goto :listen
-
-
-:open
-	if not exist "!out!" (mkdir "!out!")
-	
-	explorer "!out!"
-	
-	goto :end
 
 
 :end
