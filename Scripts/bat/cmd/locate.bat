e74f422cc76b92680a4f0b9c11b61b8729bd9295
Zachary Gill <zachary.w.gill@gmail.com>
2023-10-28 09:08:23 -0400

    Updated cmd locate Scripts

--- Scripts/bat/cmd/locate.bat
+++ Scripts/bat/cmd/locate.bat
@@ -1,3 +1,18 @@
 @echo off
+@setlocal enabledelayedexpansion
 
-dir /b %*
+set "flags=/b"
+set "pattern=*"
+
+:readArgs
+	if not '%1'=='' (
+		set "arg=%~1"
+		if "!arg:~0,1!"=="/" (
+			set "flags=!flags! !arg!"
+		) else if not "!arg!"=="" (
+			set "pattern=*!arg!*"
+		)
+		shift && goto readArgs
+	)
+
+dir !flags! "!pattern!"
