35dd508a2d72de9a1e4c9c1ffb88bb4ccfc1d330
Zachary Gill <zachary.w.gill@gmail.com>
2024-02-22 05:12:42 -0500

    Updated network reset Script

--- Scripts/bat/win/resetnetwork.bat
+++ Scripts/bat/win/resetnetwork.bat
@@ -7,6 +7,6 @@ ipconfig /flushdns
 ipconfig /renew
 
 netsh winsock reset
-netsh int ip reset
-netsh interface ipv4 reset
-netsh interface ipv6 reset
+:: netsh int ip reset
+:: netsh interface ipv4 reset
+:: netsh interface ipv6 reset
