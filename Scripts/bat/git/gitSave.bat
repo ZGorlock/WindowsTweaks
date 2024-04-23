fac3a84f17c6ae214588e2a391509521c6aae288
Zachary Gill <zachary.w.gill@gmail.com>
2024-04-23 06:03:23 -0400

    Refactored gitSave Script

--- Scripts/bat/git/gitSave.bat
+++ Scripts/bat/git/gitSave.bat
@@ -8,108 +8,32 @@ if '!out!'=='' (set "out=%GitsaveDir%")
 if '!out!'=='' (set "out=%Downloads%")
 if '!out!'=='' (set "out=%UserProfile%\Downloads")
 
-set listen=false
+set "source=!url:https://=!"
+set "source=!source:github.com/=github.com !"
+set "source=!source:/= ! "
+set "source=!source:.git =!"
 
-echo.
-echo --------------------------------------------------
-echo.
+for /f "tokens=1,2,3* delims=?# " %%h in ("!source!") do (
+	set "host=%%h"
+	set "user=%%i"
+	set "repo=%%j"
+)
+if '!host!'=='' (
+	echo No host specified.
+	exit /b 1
+)
+if '!user!'=='' (
+	echo No user specified.
+	exit /b 1
+)
+if '!repo!'=='' (
+	echo No repo specified.
+	exit /b 1
+)
 
-goto :main
+set "remote=https://!host!/!user!/!repo!.git"
 
+set "local=!out!\!user! ]] !repo!"
+set "local=!local:\\=\!"
 
-:main
-	call :command
-	call :process
-	
-	goto :input
-
-
-:input
-	if not '!listen!'=='true' (goto :end)
-	
-	echo.
-	echo --------------------------------------------------
-	echo.
-	
-	set url=
-	set /p url=: 
-	
-	goto :main
-
-
-:command
-	if '!url!'=='start'   (call :start)
-	if '!url!'=='server'  (call :start)
-	if '!url!'=='listen'  (call :start)
-	if '!url!'=='loop'    (call :start)
-	
-	if '!url!'=='stop'    (call :stop)
-	if '!url!'=='quit'    (call :stop)
-	if '!url!'=='exit'    (call :stop)
-	if '!url!'=='end'     (call :stop)
-	if '!url!'=='break'   (call :stop)
-	if '!url!'==''        (call :stop)
-	
-	if '!url!'=='open'    (call :open)
-	if '!url!'=='explore' (call :open)
-	if '!url!'=='pop'     (call :open)
-	if '!url!'=='dir'     (call :open)
-	
-	exit /b 0
-
-
-:start
-	if '!listen!'=='true' (exit /b 1)
-	
-	echo Listening...
-	set listen=true
-	
-	exit /b 0
-
-
-:stop
-	if '!listen!'=='false' (exit /b 1)
-	
-	echo Done
-	set listen=false
-	
-	exit /b 0
-
-
-:open
-	if not exist "!out!" (mkdir "!out!")
-	
-	echo !out!
-	explorer "!out!"
-	
-	exit /b 0
-
-
-:process
-	if '!url!'==''          (exit /b 1)
-	if '!url!'=='!url://=!' (exit /b 1)
-	
-	if not exist "!out!" (mkdir "!out!")
-	cd /D "!out!"
-	
-	call :download
-	
-	exit /b 0
-
-
-:download
-	set "repo=!url:https://github.com/=!"
-	set "repo=!repo:.git=!"
-	set "repo=!repo:/= ]] !"
-	set "repo=!out!\!repo!"
-	set "repo=!repo:\\=\!"
-	
-	git clone "!url!" "!repo!"
-	
-	exit /b 0
-
-
-:end
-	echo.
-	echo --------------------------------------------------
-	echo.
+git clone "!remote!" "!local!"
